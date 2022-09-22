ae65e3104c6fd2a2e2eb1215e3454cc7f91fb853
Serhii Hromov <melifarowow@gmail.com>
2022-09-22 12:32:33 +0300

    isLocal fix

--- scripts/articles.js
+++ scripts/articles.js
@@ -4,7 +4,7 @@ function isLocal() {
     return location.hostname === "localhost" || location.hostname === "127.0.0.1";
 }
 
-const baseURL = isLocal ? '' : 'https://hromov.github.io/triangled_theme_switcher';
+const baseURL = isLocal() ? '' : 'https://hromov.github.io/triangled_theme_switcher';
 
 export async function getArticle(articleID) {
     const resp = await fetch(`${baseURL}/fake_api/${articleID}.json`);
